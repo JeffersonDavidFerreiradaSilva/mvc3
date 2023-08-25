8d115f2ced4420821976ee2171be1d84ed5ce65d
Jefferson David <napsterdavid@gmail.com>
2023-08-25 15:56:30 -0300

    Pagina que obtem os dados pronto

---
+++ view/pagina_cadastro_aluno.py
@@ -0,0 +1,10 @@
+class cadastro:
+    @staticmethod
+    def pegar_dados_aluno():
+        nome = input("DIGITE O NOME DO ALUNO: ")
+        idade = int(input("DIGITE A IDADE DO ALUNO: "))
+        peso = float(input("DIGITE O PESO DO ALUNO: "))
+        return nome,idade,peso
+    @staticmethod
+    def mostrar_menssagem(menssagem):
+        print(menssagem)
